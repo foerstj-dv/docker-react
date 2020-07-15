fb956333810fcfe7376653847b993a2317a7c9e2
Johannes Förstner <johannes.foerstner@debtvision.de>
2020-07-15 09:58:12 +0200

    Yo.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Yo!
+          Yo.
         </p>
         <a>learn react</a>
       </header>
